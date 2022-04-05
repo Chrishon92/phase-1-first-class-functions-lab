56832e409e8af63132b5df80652e12aeed0a40a0
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-04-04 20:45:26 -0500

    commit tree 8523b4a581b78f6f830f7a0c8e3a07f474de1089

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+let drivers = ["Antonia","Nuru","Amari","Mo"]
+
+const returnFirstTwoDrivers = () => {
+    return drivers = ["Antonia", "Nuru"]
+} 
+
+const returnLastTwoDrivers = () => {
+    return drivers = ["Amari", "Mo"]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = () => {
+    
+}
